761debf8ad32983ccf3c9492963a144662778d54
imorales1 <alejan_dro-morales@hotmail.com>
2022-12-12 23:45:28 -0600

    Primet Commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIS_con_.Net.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+
+        if(ListWeatherForecast == null || !ListWeatherForecast.Any()) {
+
+            ListWeatherForecast =  Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+
+        }
+
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    [Route("Get/weatherforecast")]
+    [Route("Get/weatherforecast1")]
+    //[Route("[action]")]
+    public IEnumerable<WeatherForecast> Get()
+    { 
+        return ListWeatherForecast;
+    }
+
+    [HttpPost]
+    [Route("Post/weatherforecastAdd")]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        try {
+
+            ListWeatherForecast.Add(weatherForecast);
+
+        }catch(Exception ex) {
+            return Ok("Ha ocurrido un error: " + ex);
+        }
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        try {
+
+            ListWeatherForecast.RemoveAt(index);
+        
+        }catch(Exception ex){
+            return Ok("El indice proporcionado no existe en la lista, " + ex.Message);
+        }
+
+        return Ok("Registro eliminado con éxito!!!");
+    }
+}
---
+++ MiddleWares/TimeMiddleWare.cs
@@ -0,0 +1,32 @@
+public class TimeMiddleware
+{
+    readonly RequestDelegate next;
+
+    public TimeMiddleware(RequestDelegate nextRequest)
+    {
+        next = nextRequest;
+    }
+
+    public async Task Invoke(Microsoft.AspNetCore.Http.HttpContext context)
+    {
+        await next(context);
+
+        if(context.Request.Query.Any(p=> p.Key == "time"))
+        {
+            await context.Response.WriteAsync(DateTime.Now.ToShortTimeString());
+        }else{
+            await context.Response.WriteAsync("NO HAY HORA!!");
+        }
+
+    }
+
+}
+
+
+    public static class TimeMiddlewareExtension
+    {
+        public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TimeMiddleware>();
+        }
+    }
